edae470b474b9af331b04d944b51308b37d4ee79
aedot <ether.ipgage@proton.me>
2026-01-22 14:23:28 -0800

    chore: core upgrade

--- apps/m4b-tool/container_test.go
+++ apps/m4b-tool/container_test.go
@@ -2,26 +2,13 @@ package main
 
 import (
 	"context"
-	"os"
 	"testing"
 
-	"github.com/stretchr/testify/require"
-
-	"github.com/testcontainers/testcontainers-go"
+	"github.com/aedot/containers/testhelpers"
 )
 
 func Test(t *testing.T) {
 	ctx := context.Background()
-
-	image := os.Getenv("TEST_IMAGE")
-	if image == "" {
-		image = "ghcr.io/aedot/m4b-tool:rolling"
-	}
-
-	app, err := testcontainers.Run(
-		ctx, image,
-		testcontainers.WithCmdArgs("test", "-f", "/etc/os-release"),
-	)
-	testcontainers.CleanupContainer(t, app)
-	require.NoError(t, err)
+	image := testhelpers.GetTestImage("ghcr.io/aedot/m4b-tool:rolling")
+	testhelpers.TestFileExists(t, ctx, image, "/runscript.sh", nil)
 }
